14d93d216a52ff7e0ef567598d443e258b503447
zio-lab <267018090+zio-lab@users.noreply.github.com>
2026-04-02 02:25:10 +0900

    ✨ FEAT: Add hover UI to movie card

--- week03/mission01/src/components/MovieCard.tsx
+++ week03/mission01/src/components/MovieCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { Movie } from "../types/movie";
 
 interface MovieCardProps {
@@ -5,13 +6,27 @@ interface MovieCardProps {
 }
 
 export default function MovieCard({ movie }: MovieCardProps) {
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
-    <div className=' '>
+    <div 
+        className='relative cursor-pointer rounded-lg overflow-hidden shadow-lg w-44 
+        transition-transform duration-500 transform hover:scale-105'
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+    >
         <img 
             src = {`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
             alt={` ${movie.title}영화의 이미지 `}
             className='' 
         /> 
+        {isHovered && (
+            <div className='absolute inset-0 bg-gradient-to-t from-black/50 to-transparent backdrop-blur-sm
+            flex flex-col justify-center items-center text-white p-4'>
+                <h2 className='text-xl font-bold  leading-snug mb-2'>{movie.title}</h2>
+                <p className='text-sm text-gray-300 leading-relaxed mt-2 line-clamp-5'>{movie.overview}</p>
+            </div>
+        )}
     </div>
     )
 }    
